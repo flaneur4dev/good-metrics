e0b87c74848e3b8f842172bd96f40f76d205485f
Alex Basmanov <alba4ex@gmail.com>
2023-03-28 17:36:39 +0300

    increment 4/newest refactoring after autotest

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -20,26 +20,28 @@ type (
 		AllMetrics() ([]string, []string)
 	}
 	Metric interface {
-		OneMetric(t, n string) (string, error)
+		// OneMetric(t, n string) (string, error)
+		OneMetric(t, n string) (cs.Metrics, error)
 	}
 	Updater interface {
-		Update(t, n, v string) (string, error)
+		// Update(t, n, v string) (string, error)
+		Update(n string, nm cs.Metrics) (cs.Metrics, error)
 	}
 )
 
-func setMetric(res *cs.Metrics, v string) {
-	if res.MType == "gauge" {
-		f, _ := strconv.ParseFloat(v, 64)
-		value := cs.Gauge(f)
-		res.Value = &value
-		res.Delta = nil
-	} else {
-		d, _ := strconv.ParseInt(v, 10, 64)
-		value := cs.Counter(d)
-		res.Value = nil
-		res.Delta = &value
-	}
-}
+// func setMetric(res *cs.Metrics, v string) {
+// 	if res.MType == "gauge" {
+// 		f, _ := strconv.ParseFloat(v, 64)
+// 		value := cs.Gauge(f)
+// 		res.Value = &value
+// 		res.Delta = nil
+// 	} else {
+// 		d, _ := strconv.ParseInt(v, 10, 64)
+// 		value := cs.Counter(d)
+// 		res.Value = nil
+// 		res.Delta = &value
+// 	}
+// }
 
 func HandleUpdate(rep Updater) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -47,7 +49,30 @@ func HandleUpdate(rep Updater) http.HandlerFunc {
 		mName := chi.URLParam(r, "mName")
 		mValue := chi.URLParam(r, "mValue")
 
-		_, err := rep.Update(mType, mName, mValue)
+		res := cs.Metrics{ID: mName, MType: mType}
+
+		switch mType {
+		case "gauge":
+			f, err := strconv.ParseFloat(mValue, 64)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			value := cs.Gauge(f)
+			res.Value = &value
+		case "counter":
+			d, err := strconv.ParseInt(mValue, 10, 64)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			value := cs.Counter(d)
+			res.Delta = &value
+		}
+
+		_, err := rep.Update(res.ID, res)
 		if err != nil {
 			sc := http.StatusBadRequest
 			if errors.Is(err, e.ErrUnkownMetricType) {
@@ -76,15 +101,7 @@ func HandleUpdateJSON(rep Updater) http.HandlerFunc {
 			return
 		}
 
-		var value string
-		if res.Value != nil && res.Delta == nil {
-			value = fmt.Sprintf("%f", *res.Value)
-		}
-		if res.Delta != nil && res.Value == nil {
-			value = fmt.Sprintf("%d", *res.Delta)
-		}
-
-		v, err := rep.Update(res.MType, res.ID, value)
+		v, err := rep.Update(res.ID, res)
 		if err != nil {
 			sc := http.StatusBadRequest
 			if errors.Is(err, e.ErrUnkownMetricType) {
@@ -94,9 +111,7 @@ func HandleUpdateJSON(rep Updater) http.HandlerFunc {
 			return
 		}
 
-		setMetric(&res, v)
-
-		b, err := json.Marshal(res)
+		b, err := json.Marshal(v)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -123,8 +138,16 @@ func HandleMetric(rep Metric) http.HandlerFunc {
 			return
 		}
 
+		var rv string
+		switch mType {
+		case "gauge":
+			rv = fmt.Sprintf("%.3f", *v.Value)
+		case "counter":
+			rv = fmt.Sprintf("%d", *v.Delta)
+		}
+
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(v))
+		w.Write([]byte(rv))
 	}
 }
 
@@ -153,9 +176,9 @@ func HandleMetricJSON(rep Metric) http.HandlerFunc {
 			return
 		}
 
-		setMetric(&res, v)
+		// setMetric(&res, v)
 
-		b, err := json.Marshal(res)
+		b, err := json.Marshal(v)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -221,3 +244,157 @@ const tmpl = `
 	</body>
 </html>
 `
+
+// func HandleUpdate(rep Updater) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		mType := chi.URLParam(r, "mType")
+// 		mName := chi.URLParam(r, "mName")
+// 		mValue := chi.URLParam(r, "mValue")
+
+// 		_, err := rep.Update(mType, mName, mValue)
+// 		if err != nil {
+// 			sc := http.StatusBadRequest
+// 			if errors.Is(err, e.ErrUnkownMetricType) {
+// 				sc = http.StatusNotImplemented
+// 			}
+// 			http.Error(w, err.Error(), sc)
+// 			return
+// 		}
+
+// 		w.WriteHeader(http.StatusOK)
+// 	}
+// }
+
+// func HandleUpdateJSON(rep Updater) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		reqBody, err := io.ReadAll(r.Body)
+// 		if err != nil {
+// 			http.Error(w, err.Error(), http.StatusBadRequest)
+// 			return
+// 		}
+
+// 		var res cs.Metrics
+// 		err = json.Unmarshal(reqBody, &res)
+// 		if err != nil {
+// 			http.Error(w, err.Error(), http.StatusBadRequest)
+// 			return
+// 		}
+
+// 		var value string
+// 		if res.Value != nil && res.Delta == nil {
+// 			value = fmt.Sprintf("%f", *res.Value)
+// 		}
+// 		if res.Delta != nil && res.Value == nil {
+// 			value = fmt.Sprintf("%d", *res.Delta)
+// 		}
+
+// 		v, err := rep.Update(res.MType, res.ID, value)
+// 		if err != nil {
+// 			sc := http.StatusBadRequest
+// 			if errors.Is(err, e.ErrUnkownMetricType) {
+// 				sc = http.StatusNotImplemented
+// 			}
+// 			http.Error(w, err.Error(), sc)
+// 			return
+// 		}
+
+// 		setMetric(&res, v)
+
+// 		b, err := json.Marshal(res)
+// 		if err != nil {
+// 			http.Error(w, err.Error(), http.StatusInternalServerError)
+// 			return
+// 		}
+
+// 		w.Header().Set("Content-Type", "application/json")
+// 		w.WriteHeader(http.StatusOK)
+// 		w.Write(b)
+// 	}
+// }
+
+// func HandleMetric(rep Metric) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		mType := chi.URLParam(r, "mType")
+// 		mName := chi.URLParam(r, "mName")
+
+// 		v, err := rep.OneMetric(mType, mName)
+// 		if err != nil {
+// 			sc := http.StatusNotFound
+// 			if errors.Is(err, e.ErrUnkownMetricType) {
+// 				sc = http.StatusNotImplemented
+// 			}
+// 			http.Error(w, err.Error(), sc)
+// 			return
+// 		}
+
+// 		w.WriteHeader(http.StatusOK)
+// 		w.Write([]byte(v))
+// 	}
+// }
+
+// func HandleMetricJSON(rep Metric) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		reqBody, err := io.ReadAll(r.Body)
+// 		if err != nil {
+// 			http.Error(w, err.Error(), http.StatusBadRequest)
+// 			return
+// 		}
+
+// 		var res cs.Metrics
+// 		err = json.Unmarshal(reqBody, &res)
+// 		if err != nil {
+// 			http.Error(w, err.Error(), http.StatusBadRequest)
+// 			return
+// 		}
+
+// 		v, err := rep.OneMetric(res.MType, res.ID)
+// 		if err != nil {
+// 			sc := http.StatusNotFound
+// 			if errors.Is(err, e.ErrUnkownMetricType) {
+// 				sc = http.StatusNotImplemented
+// 			}
+// 			http.Error(w, err.Error(), sc)
+// 			return
+// 		}
+
+// 		setMetric(&res, v)
+
+// 		b, err := json.Marshal(res)
+// 		if err != nil {
+// 			http.Error(w, err.Error(), http.StatusInternalServerError)
+// 			return
+// 		}
+
+// 		w.Header().Set("Content-Type", "application/json")
+// 		w.WriteHeader(http.StatusOK)
+// 		w.Write(b)
+// 	}
+// }
+
+// func HandleMetrics(rep Metrics) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		gm, cm := rep.AllMetrics()
+
+// 		t, err := template.New("metrics webpage").Parse(tmpl)
+// 		if err != nil {
+// 			http.Error(w, "Somthing went wrong", http.StatusInternalServerError)
+// 			return
+// 		}
+
+// 		data := struct {
+// 			Title   string
+// 			GMetics []string
+// 			CMetics []string
+// 		}{
+// 			Title:   "Good metrics",
+// 			GMetics: gm,
+// 			CMetics: cm,
+// 		}
+
+// 		err = t.Execute(w, data)
+// 		if err != nil {
+// 			http.Error(w, "Somthing went wrong", http.StatusInternalServerError)
+// 			return
+// 		}
+// 	}
+// }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,82 +2,129 @@ package storage
 
 import (
 	"fmt"
-	"strconv"
+	// "strconv"
 
 	cs "github.com/flaneur4dev/good-metrics/internal/contracts"
 	e "github.com/flaneur4dev/good-metrics/internal/lib/mistakes"
 )
 
-type MemStorage struct {
-	gauge   map[string]cs.Gauge
-	counter map[string]cs.Counter
+// type MemStorage struct {
+// 	gauge   map[string]cs.Gauge
+// 	counter map[string]cs.Counter
+// }
+
+type MemStorage map[string]cs.Metrics
+
+// func New() *MemStorage {
+// 	return &MemStorage{
+// 		gauge:   map[string]cs.Gauge{},
+// 		counter: map[string]cs.Counter{},
+// 	}
+// }
+
+func New() MemStorage {
+	return MemStorage{}
 }
 
-func New() *MemStorage {
-	return &MemStorage{
-		gauge:   map[string]cs.Gauge{},
-		counter: map[string]cs.Counter{},
-	}
-}
-
-func (ms *MemStorage) AllMetrics() ([]string, []string) {
-	gm := make([]string, len(ms.gauge))
-	cm := make([]string, len(ms.counter))
-
-	i := 0
-	for k, v := range ms.gauge {
-		gm[i] = fmt.Sprintf("%s: %f", k, v)
-		i++
-	}
-
-	i = 0
-	for k, v := range ms.counter {
-		cm[i] = fmt.Sprintf("%s: %d", k, v)
-		i++
-	}
-
-	return gm, cm
-}
-
-func (ms *MemStorage) OneMetric(t, n string) (string, error) {
-	switch t {
-	case "gauge":
-		v, ok := ms.gauge[n]
-		if !ok {
-			return "", e.ErrNoMetric
+func (ms MemStorage) AllMetrics() (gm, cm []string) {
+	for k, v := range ms {
+		if v.MType == "gauge" {
+			gm = append(gm, fmt.Sprintf("%s: %f", k, *v.Value))
+		} else {
+			cm = append(cm, fmt.Sprintf("%s: %d", k, *v.Delta))
 		}
-		return fmt.Sprintf("%f", v), nil
-	case "counter":
-		v, ok := ms.counter[n]
-		if !ok {
-			return "", e.ErrNoMetric
-		}
-		return fmt.Sprintf("%d", v), nil
-	default:
-		return "", e.ErrUnkownMetricType
 	}
+	return
 }
 
-func (ms *MemStorage) Update(t, n, v string) (string, error) {
-	switch t {
-	case "gauge":
-		f, err := strconv.ParseFloat(v, 64)
-		if err != nil {
-			return "", e.ErrInvalidData
-		}
-
-		ms.gauge[n] = cs.Gauge(f)
-		return v, nil
-	case "counter":
-		i, err := strconv.ParseInt(v, 10, 64)
-		if err != nil {
-			return "", e.ErrInvalidData
-		}
-
-		ms.counter[n] += cs.Counter(i)
-		nv := ms.counter[n]
-		return fmt.Sprintf("%d", nv), nil
-	default:
-		return "", e.ErrUnkownMetricType
+func (ms MemStorage) OneMetric(t, n string) (cs.Metrics, error) {
+	m, ok := ms[n]
+	if !ok || m.MType != t {
+		return cs.Metrics{}, e.ErrNoMetric
 	}
+	return m, nil
 }
+
+func (ms MemStorage) Update(n string, nm cs.Metrics) (cs.Metrics, error) {
+	if n == "" {
+		return cs.Metrics{}, e.ErrInvalidData
+	}
+
+	if !(nm.MType == "gauge" && nm.Value != nil) && !(nm.MType == "counter" && nm.Delta != nil) {
+		return cs.Metrics{}, e.ErrUnkownMetricType
+	}
+
+	if nm.MType == "counter" {
+		if m, ok := ms[n]; ok {
+			nv := *m.Delta + *nm.Delta
+			m.Delta = &nv
+			ms[n] = m
+			return m, nil
+		}
+	}
+
+	ms[n] = nm
+	return nm, nil
+}
+
+// func (ms *MemStorage) AllMetrics() ([]string, []string) {
+// 	gm := make([]string, len(ms.gauge))
+// 	cm := make([]string, len(ms.counter))
+
+// 	i := 0
+// 	for k, v := range ms.gauge {
+// 		gm[i] = fmt.Sprintf("%s: %f", k, v)
+// 		i++
+// 	}
+
+// 	i = 0
+// 	for k, v := range ms.counter {
+// 		cm[i] = fmt.Sprintf("%s: %d", k, v)
+// 		i++
+// 	}
+
+// 	return gm, cm
+// }
+
+// func (ms *MemStorage) OneMetric(t, n string) (string, error) {
+// 	switch t {
+// 	case "gauge":
+// 		v, ok := ms.gauge[n]
+// 		if !ok {
+// 			return "", e.ErrNoMetric
+// 		}
+// 		return fmt.Sprintf("%f", v), nil
+// 	case "counter":
+// 		v, ok := ms.counter[n]
+// 		if !ok {
+// 			return "", e.ErrNoMetric
+// 		}
+// 		return fmt.Sprintf("%d", v), nil
+// 	default:
+// 		return "", e.ErrUnkownMetricType
+// 	}
+// }
+
+// func (ms *MemStorage) Update(t, n, v string) (string, error) {
+// 	switch t {
+// 	case "gauge":
+// 		f, err := strconv.ParseFloat(v, 64)
+// 		if err != nil {
+// 			return "", e.ErrInvalidData
+// 		}
+
+// 		ms.gauge[n] = cs.Gauge(f)
+// 		return v, nil
+// 	case "counter":
+// 		i, err := strconv.ParseInt(v, 10, 64)
+// 		if err != nil {
+// 			return "", e.ErrInvalidData
+// 		}
+
+// 		ms.counter[n] += cs.Counter(i)
+// 		nv := ms.counter[n]
+// 		return fmt.Sprintf("%d", nv), nil
+// 	default:
+// 		return "", e.ErrUnkownMetricType
+// 	}
+// }
